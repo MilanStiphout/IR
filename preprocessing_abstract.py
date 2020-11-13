104b2c456fc138e50462916e6bf0128521fc6378
Lennart Geertjes <lennartgeertjes@gmail.com>
2020-11-13 15:07:31 +0100

    made title and abstract changes

--- preprocessing_abstract.py
+++ preprocessing_abstract.py
@@ -2,26 +2,35 @@ import json
 import csv
 import os
 import nltk
+from nltk.stem import PorterStemmer
 import itertools
 from collections import defaultdict
 from string import punctuation
 
-def get_file():
-    with open('Data\CORD-19\CORD-19\metadata.csv', encoding="utf8") as f_in:
+def get_files():
+    #create an instance of the algorithm which will do the stemming
+    porter_stemmer = PorterStemmer()
+    #open the metadata file
+    with open('C:/Users/Lennart Geertjes/IR_repo/IR/CORD-19/metadata.csv', encoding="utf8") as f_in:
         reader = csv.DictReader(f_in)
-        header = itertools.islice(reader, 2)
-        for row in header:
-            #access metadata
-            cord_uid = row['cord_uid'] #returns a string
-            title = row['title'] #returns a string
-            abstract = row['abstract'] #returns a string
+        header = itertools.islice(reader, 2) 
+        for row in header: #when iterating over the entire dataset, use reader
+            #get the title and change it into BoW representation
+            title = row['title'] 
+            tokenized_title = get_tokens(title)
+            stemmed_title = get_stems(tokenized_title, porter_stemmer)
 
-            tokenized_abstract = get_tokens(abstract)
-            print(tokenized_abstract, end = "\n\n")
+            #get the abstract from metadata.csv and change it into BoW representation
+            abstract_text = row['abstract'] 
+            tokenized_abstract = get_tokens(abstract_text)
+            stemmed_abstract = get_stems(tokenized_abstract, porter_stemmer)
+            
+            stemmed_abstract.extend(stemmed_title)
+            print(stemmed_abstract)
 
-def get_tokens(paragraph_text):
+def get_tokens(text):
     #tokenize the data
-    tokenized_text = nltk.word_tokenize(paragraph_text)
+    tokenized_text = nltk.word_tokenize(text)
     #make a list of stopwords that should be removed from the data
     words_to_remove = nltk.corpus.stopwords.words("english")
     #add punctuation to list of words that should be removed
@@ -29,10 +38,12 @@ def get_tokens(paragraph_text):
     #returns set of tokens that are not in words_to_remove
     return [t for t in set(tokenized_text) if not t in words_to_remove]
 
+def get_stems(text, stemmer):
+    return[stemmer.stem(t) for t in text]
 
 if __name__ == "__main__":
     #download necessary elements of nltk module
     nltk.download('punkt')
     nltk.download('stopwords')
-    get_file()
+    get_files()
 
--- preprocessing_full_text.py
+++ preprocessing_full_text.py
@@ -7,34 +7,42 @@ import itertools
 from collections import defaultdict
 from string import punctuation
 
-def get_file():
-    word_stemmer = PorterStemmer()
-    with open('Data\CORD-19\CORD-19\metadata.csv', encoding="utf8") as f_in:
+def get_files():
+    #create an instance of the algorithm which will do the stemming
+    porter_stemmer = PorterStemmer()
+    #open the metadata file
+    with open('C:/Users/Lennart Geertjes/IR_repo/IR/CORD-19/metadata.csv', encoding="utf8") as f_in:
         reader = csv.DictReader(f_in)
         header = itertools.islice(reader, 1)
-        for row in header:
-            #access metadata
-            cord_uid = row['cord_uid'] #returns a string
-            title = row['title'] #returns a string
-            abstract = row['abstract'] #returns a string #abstract nog tokenizen en voor de full_text plakken
+        for row in header: #when iterating over the entire dataset, use reader
+            full_stemmed_text = []
+            #get the title and change it into BoW representation
+            title = row['title'] 
+            tokenized_title = get_tokens(title)
+            stemmed_title = get_stems(tokenized_title, porter_stemmer)
+
+            #get the abstract from metadata.csv and change it into BoW representation
+            abstract_text = row['abstract'] 
+            tokenized_abstract = get_tokens(abstract_text)
+            stemmed_abstract = get_stems(tokenized_abstract, porter_stemmer)
 
             #access full text 
             if row['pdf_json_files']:
                 for json_path in row['pdf_json_files'].split('; '):
-                    with open('Data/CORD-19/CORD-19/' + json_path) as f_json:
+                    with open('C:/Users/Lennart Geertjes/IR_repo/IR/CORD-19/' + json_path) as f_json:
                         full_text_dict = json.load(f_json) 
 
                         #grab parts of the text 
-                        full_text = []
                         for paragraphs in full_text_dict['body_text']:
                             tokenized_paragraph = get_tokens(paragraphs['text'])
-                            stemmed_paragraph = stem_tokens(tokenized_paragraph, word_stemmer)
-                            full_text.extend(stemmed_paragraph)
-                        print(full_text)
+                            stemmed_paragraph = get_stems(tokenized_paragraph, porter_stemmer)
+                            full_stemmed_text.extend(stemmed_paragraph)
+                        print(full_stemmed_text)
+        full_stemmed_text.extend([stemmed_title, stemmed_abstract])            
 
-def get_tokens(paragraph_text):
+def get_tokens(text):
     #tokenize the data
-    tokenized_text = nltk.word_tokenize(paragraph_text)
+    tokenized_text = nltk.word_tokenize(text)
     #make a list of stopwords that should be removed from the data
     words_to_remove = nltk.corpus.stopwords.words("english")
     #add punctuation to list of words that should be removed
@@ -42,18 +50,12 @@ def get_tokens(paragraph_text):
     #returns set of tokens that are not in words_to_remove
     return [t for t in tokenized_text if not t in words_to_remove]
 
-def stem_tokens(tokenized_text, word_stemmer):
-    return[word_stemmer(t) for t in tokenized_text]
-    '''
-    #stem the tokens
-    stemmed_tokens = []
-    for token in tokenized_text:
-        stemmed_tokens.extend(word_stemmer(token))
-    '''
+def get_stems(text, stemmer):
+    return[stemmer.stem(t) for t in text]
 
 if __name__ == "__main__":
     #download necessary elements of nltk module
     nltk.download('punkt')
     nltk.download('stopwords')
-    get_file()
+    get_files()
 
