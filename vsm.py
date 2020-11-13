2bfdb69086d9c01bfb5967fb342894fc15206140
Milan <milanstiphout@gmail.com>
2020-11-13 08:45:31 +0100

    Finished TF-IDF

--- vsm.py
+++ vsm.py
@@ -12,3 +12,9 @@ def idf(word, doc, docs):
     return math.log((len(docs) + 1)/df(word, docs))
 
 
+def tf_idf(query, vocabulary, docs):
+    doc_scores = []
+    for d in docs:
+        doc_scores.append(sum([d[w] * idf(w, d, docs) for w in query if w in d]))
+    return doc_scores
+
