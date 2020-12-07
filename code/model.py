a459108dcb0cb7525c97172bd68f3cad78b28c16
milanstiphout@gmail.com <milanstiphout@gmail.com>
2020-12-07 21:32:38 +0100

    Preliminary work on conversion of file to BoW-model. Untested.

--- code/model.py
+++ code/model.py
@@ -1,13 +1,33 @@
-def get_vocabulary(docs):
+import os
+from collections import defaultdict
+
+def index_word(word, vocab, inverse_list, d, f):
+    if word not in vocab:
+        vocab.append(word)
+    if word in inverse_list:
+        inverse_list[word] += [f['id']]
+    else:
+        inverse_list[word] = [f['id']]
+    d[word] += 1
+    return vocab, inverse_list, d
+
+
+def build_model():
     vocab = []
-    for d in docs:
-        for word in d:
-            vocab.append(word)
-    return set(vocab)
-
-
-def inverse_list(vocab, docs):
     inverse_list = {}
-    for word in vocab:
-        inverse_list[word] = [d for d in docs if word in d]
-    return inverse_list
+    file_dicts = []
+    with open(os.getcwd() + '/CORD-19/preprocessed.csv', encoding='utf8') as file_list:
+        for f in file_list:
+            d = defaultdict(int)
+            d['file_id'] = f['id']
+            for word in f['title'] + f['abstract']:
+                vocab, inverse_list, d = index_word(word, vocab, inverse_list, d, f)
+            # Save this abstract-only BoW model somewhere
+            for word in f['fulltext']:
+                vocab, inverse_list, d = index_word(word, vocab, inverse_list, d, f)
+                file_dicts.append(d)
+    return vocab, inverse_list, file_dicts
+    # Write vocab, inverse_list and file_dicts somewhere
+
+if __name__ == "__main__":
+    build_model()
