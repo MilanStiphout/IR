5bb11fc19a7ac270134ab481a888a27bc76d60f9
milanstiphout@gmail.com <milanstiphout@gmail.com>
2020-12-10 09:10:45 +0100

    Small change to idf() to make it work with inverse list

--- code/vsm.py
+++ code/vsm.py
@@ -1,20 +1,21 @@
 import math
 
-def tf(word, doc):
-    return doc[word] if word in doc else 0
+def tf(term, doc):
+    return doc[term] if term in doc else 0
 
 
-def df(word, docs):
-    return sum([1 for d in docs if tf(word, d) > 0])
+def df(term, inv_list):
+    return len(inv_list[term]) if term in inv_list else 0
             
 
-def idf(word, doc, docs):
-    return math.log((len(docs) + 1)/df(word, docs))
+def idf(term, docs, inv_list):
+    return math.log((len(docs) + 1)/df(term, inv_list))
 
 
-def tf_idf(query, docs):
+# TODO: compatible maken met uiteindelijke querying
+def tf_idf(query, docs, inv_list):
     doc_scores = []
     for d in docs:
-        doc_scores.append(sum([d[w] * idf(w, d, docs) for w in query if w in d]))
+        doc_scores.append(sum([d[w] * idf(w, docs, inv_list) for w in query if w in d]))
     return doc_scores
 
