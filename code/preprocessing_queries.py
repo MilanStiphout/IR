312066ca20da2aab67fce632dd060534fa923be8
Lennart Geertjes <lennartgeertjes@gmail.com>
2020-12-10 13:21:07 +0100

    preprocessed queries are now saved in csv

--- code/preprocessing_queries.py
+++ code/preprocessing_queries.py
@@ -6,24 +6,17 @@ import preprocessing_functions as pf
 
 
 def get_file():
-    with open('C:/Users/Lennart Geertjes/IR_repo/IR/topics-rnd3.csv', encoding = "utf8") as f_in:
+    with open(os.getcwd() + '/topics-rnd3.csv', encoding = "utf8") as f_in:
         reader = csv.DictReader(f_in)
-        query_dict = dict()
-        for row in reader:
-            tokenized_query = pf.get_tokens(row['query'])
-            tokenized_question = pf.get_tokens(row['question'])
-            tokenized_narrative = pf.get_tokens(row['narrative'])
-
-            stemmed_query = pf.get_stems(tokenized_query)
-            stemmed_question = pf.get_stems(tokenized_question)
-            stemmed_narrative = pf.get_stems(tokenized_narrative)
-
-            query_dict.update({row['topic-id'] : {'query' : stemmed_query, 
-                                                'question' : stemmed_question,
-                                                'narrative' : stemmed_narrative
-                                                }
-                            })
-        #print(query_dict)
+        with open(os.getcwd() + '/CORD-19/preprocessed_queries.csv', mode= 'w', encoding="utf8") as new_file:
+            writer = csv.DictWriter(new_file, fieldnames=['id', 'query', 'question', 'narrative']) 
+            writer.writeheader()
+            for row in reader: 
+                writer.writerow({'id' : row['topic-id'], 
+                                'query' : pf.preprocess(row['query']), 
+                                'question' : pf.preprocess(row['query']),
+                                'narrative' : pf.preprocess(row['query'])                                         
+                                })
 
 if __name__ == "__main__":
     get_file()
