54c18b6da7c13c8fce62868ed12a50b369b176e1
milanstiphout@gmail.com <milanstiphout@gmail.com>
2020-12-11 09:57:10 +0100

    Further work on fixing TF.IDF

--- code/vsm.py
+++ code/vsm.py
@@ -8,14 +8,12 @@ def df(term, inv_list):
     return len(inv_list[term]) if term in inv_list else 0
             
 
-def idf(term, docs, inv_list):
-    return math.log((len(docs) + 1)/df(term, inv_list))
+def idf(term, nr_of_docs, inv_list):
+    return math.log((nr_of_docs + 1)/df(term, inv_list))
 
 
-# TODO: compatible maken met uiteindelijke querying
-def tf_idf(query, docs, inv_list):
-    doc_scores = []
-    for d in docs:
-        doc_scores.append(sum([d[w] * idf(w, docs, inv_list) for w in query if w in d]))
-    return doc_scores
-
+def tf_idf(query, bow, inv_list):
+    score = 0
+    for term in query:
+        score += query[term] * tf(term, bow) * idf(term, len(bow), inv_list) 
+    return score
