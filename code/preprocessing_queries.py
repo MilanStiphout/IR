263349f3143f4c319fb1b09d14e69637426092f0
Lennart Geertjes <lennartgeertjes@gmail.com>
2020-12-11 16:32:07 +0100

    mediocre query preprocessing

--- code/preprocessing_queries.py
+++ code/preprocessing_queries.py
@@ -3,6 +3,7 @@ import csv
 import json
 import nltk
 import preprocessing_functions as pf
+from collections import defaultdict
 
 
 def get_file():
@@ -12,11 +13,20 @@ def get_file():
             writer = csv.DictWriter(new_file, fieldnames=['id', 'query', 'question', 'narrative']) 
             writer.writeheader()
             for row in reader: 
+                
                 writer.writerow({'id' : row['topic-id'], 
-                                'query' : pf.preprocess(row['query']), 
-                                'question' : pf.preprocess(row['query']),
-                                'narrative' : pf.preprocess(row['query'])                                         
+                                'query' :  pf.update_dict(defaultdict(int), pf.preprocess(row['query'])), 
+                                'question' : pf.update_dict(defaultdict(int), pf.preprocess(row['question'])),
+                                'narrative' : pf.update_dict(defaultdict(int), pf.preprocess(row['narrative']))                                         
                                 })
+        
+    with open(os.getcwd()+ '/CORD-19/preprocessed_queries.csv') as f_in:
+        reader = csv.DictReader(f_in)
+        for line in reader:
+            query = line['query']
+            print(eval(query))
+
 
 if __name__ == "__main__":
     get_file()
+
