8854352afcf436b33c17c44e6dc73498e46fc8a3
Lennart Geertjes <lennartgeertjes@gmail.com>
2020-12-19 16:48:29 +0100

    updated ranking in output

--- code/run.py
+++ code/run.py
@@ -1,6 +1,7 @@
 from vsm import tf_idf
 import os
 import csv
+from collections import OrderedDict 
 
 
 def run_vsm(model_path, results_path):
@@ -10,19 +11,24 @@ def run_vsm(model_path, results_path):
             inv_list_reader = csv.DictReader(inv_list_file)
             query_reader = csv.DictReader(query_file)
             for query_line in query_reader:
-                    with open(os.getcwd() + model_path, encoding = 'utf8') as bow_file:
-                        bow_reader = csv.DictReader(bow_file)
-                        for bow in bow_reader:
-                            score = tf_idf(eval(query_line['narrative']), eval(bow['model']), inv_list_reader)
-                           
-                            #query-id Q0 document-id rank score STANDARD
-                            res_vsm_abstract.write('{} Q0 {} {} {} STANDARD \n'.format(query_line['id'],
-                                                                                        bow['id'],
-                                                                                        'rank', #TODO: get ranking
-                                                                                        str(score)
-                                                                                        )
-                                                    )
-                           
+                scores = {}
+                rank = 1
+                with open(os.getcwd() + model_path, encoding = 'utf8') as bow_file:
+                    bow_reader = csv.DictReader(bow_file)
+                    for bow in bow_reader:
+                        score = tf_idf(eval(query_line['narrative']), eval(bow['model']), inv_list_reader)
+                        scores[bow['id']]= score
+                
+                sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse= True)
+                for [key, value] in sorted_scores:
+                #query-id Q0 document-id rank score STANDARD
+                    res_vsm_abstract.write('{} Q0 {} {} {} STANDARD \n'.format(query_line['id'],
+                                                                                key,
+                                                                                rank,
+                                                                                str(value)
+                                                                                )
+                                            )
+                    rank += 1
 
 if __name__ == "__main__":
     run_vsm("/CORD-19/abstract-model.csv", "/CORD-19/res_vsm_abstract.txt")
