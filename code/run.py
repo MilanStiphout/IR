2b323638322cd13e232b36baf5f2cc5f2be1c6f9
Lennart Geertjes <lennartgeertjes@gmail.com>
2020-12-20 14:34:04 +0100

    adjusted module to the new data format

--- code/run.py
+++ code/run.py
@@ -2,11 +2,12 @@ from vsm import tf_idf
 import os
 import csv
 from collections import OrderedDict 
+import sys
 
 
 def run_vsm(model_path, results_path):
     with open(os.getcwd() + "/CORD-19/inverse_list.csv", encoding = 'utf8') as inv_list_file, \
-         open(os.getcwd() + results_path, mode= 'w',encoding = 'utf8', newline = '') as res_vsm_abstract, \
+         open(os.getcwd() + results_path, mode= 'w',encoding = 'utf8', newline = '') as res_vsm, \
          open(os.getcwd()+ '/CORD-19/preprocessed_queries.csv', encoding = 'utf8') as query_file:
             inv_list_reader = csv.DictReader(inv_list_file)
             query_reader = csv.DictReader(query_file)
@@ -16,13 +17,13 @@ def run_vsm(model_path, results_path):
                 with open(os.getcwd() + model_path, encoding = 'utf8') as bow_file:
                     bow_reader = csv.DictReader(bow_file)
                     for bow in bow_reader:
-                        score = tf_idf(eval(query_line['narrative']), eval(bow['model']), inv_list_reader)
+                        score = tf_idf(eval(query_line['narrative']), eval(bow['abs_model']), inv_list_reader)
                         scores[bow['id']]= score
                 
                 sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse= True)
                 for [key, value] in sorted_scores:
                 #query-id Q0 document-id rank score STANDARD
-                    res_vsm_abstract.write('{} Q0 {} {} {} STANDARD \n'.format(query_line['id'],
+                    res_vsm.write('{} Q0 {} {} {} STANDARD \n'.format(query_line['id'],
                                                                                 key,
                                                                                 rank,
                                                                                 str(value)
@@ -31,5 +32,5 @@ def run_vsm(model_path, results_path):
                     rank += 1
 
 if __name__ == "__main__":
-    run_vsm("/CORD-19/abstract-model.csv", "/CORD-19/res_vsm_abstract.txt")
-    run_vsm("/CORD-19/full-model.csv", "/CORD-19/res_vsm_full_text.txt")
+    run_vsm("/CORD-19/index.csv", "/CORD-19/res_vsm_abstract.txt")
+    #run_vsm("/CORD-19/index.csv", "/CORD-19/res_vsm_full_text.txt")
